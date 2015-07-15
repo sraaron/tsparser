76098823c4ca1a3e80d246421e03a840ae65550a
leon <leon.lyy@gmail.com>
2015-07-15 15:58:12 +0800

    Remove adapt priv log

--- parser.go
+++ parser.go
@@ -13,45 +13,6 @@ import (
 
 var Root string
 
-type Logger struct {
-	Pid                   int
-	Root                  string
-	AdaptFieldPrivDataLog *os.File
-}
-
-type AdaptOut struct {
-	Pos     int64
-	Content ts.AdaptFieldPrivData
-}
-
-func newLogger(pid int, root string) *Logger {
-	logger := Logger{}
-	logger.Pid = pid
-	logger.Root = root
-	return &logger
-}
-
-func (logger *Logger) LogAdaptFieldPrivData(pkt *ts.TsPkt) {
-	if pkt.AdaptField == nil || pkt.AdaptField.PrivateData == nil {
-		return
-	}
-	data := pkt.AdaptField.PrivateData
-	if logger.AdaptFieldPrivDataLog == nil {
-		fname := filepath.Join(logger.Root, strconv.Itoa(logger.Pid)+"-tspriv.csv")
-		var err error
-		logger.AdaptFieldPrivDataLog, err = os.Create(fname)
-		if err != nil {
-			panic(err)
-		}
-	}
-	privList := ts.ParseAdaptFieldPrivData(data)
-	for _, p := range privList {
-		adaptOut := AdaptOut{pkt.Pos, p}
-		c, _ := json.Marshal(adaptOut)
-		fmt.Fprintln(logger.AdaptFieldPrivDataLog, string(c))
-	}
-}
-
 func parse(fname string, outdir string, psiOnly bool) {
 	var pkts chan *ts.TsPkt
 
@@ -83,19 +44,12 @@ func parse(fname string, outdir string, psiOnly bool) {
 	}
 
 	records := make(map[int]ts.Record)
-	loggers := make(map[int]*Logger)
 	for pid, s := range streams {
 		records[pid] = ts.CreateRecord(pid, ts.GetStreamType(s), outdir)
-		loggers[pid] = newLogger(pid, outdir)
 	}
 
 	pkts = ts.ParseFile(fname)
 	for pkt := range pkts {
-		logger := loggers[pkt.Pid]
-		if logger != nil {
-			logger.LogAdaptFieldPrivData(pkt)
-		}
-
 		if pcr, ok := pkt.PCR(); ok {
 			if pids, ok := pcrs[pkt.Pid]; ok {
 				// Save the PCR value
